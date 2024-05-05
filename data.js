3cadb4cfacfda0d9d55362559269bd29436860c8
encayni <oganisyanararat1@gmail.com>
2024-05-05 17:41:57 +0400

    first commit

---
+++ data.js
@@ -0,0 +1,16 @@
+export const stations = [
+    {
+        name: "Station Red",
+        attractions: [
+            "Red Square",
+            "Lenin Mausolei",
+        ]
+    },
+    {
+        name:"Station Lublyanka",
+        attractions: [
+            "Dinosaur",
+            "vitalik",
+        ]
+    }
+]
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Welcome from 'components/Welcome/Welcome'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Welcome/>
+    </>
+  )
+}
+
+export default App
