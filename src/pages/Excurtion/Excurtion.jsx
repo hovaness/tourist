eb986dc053328830c17d8728638cdda2046bfc92
tekerau <tekerau666@mail.ru>
2024-06-04 15:16:30 +0400

    китай-город

--- src/pages/Excurtion/Excurtion.jsx
+++ src/pages/Excurtion/Excurtion.jsx
@@ -1,56 +1,51 @@
-import { Carousel } from 'antd';
-import LogoWithNav from 'components/LogoWithNav/LogoWithNav';
 import NavButton from 'components/NavButton/NavButton';
-import Player from 'components/Player/Player';
 import tours from 'data';
 import styles from 'pages/Excurtion/exucrtion.module.css'
 import { useParams } from 'react-router-dom'
 
+
 function Excurtion() {
     const { tourId, excursionId } = useParams();
     const currExc = tours.get(tourId).excurtions.find((exc) => exc.slug === excursionId)
-    const contentStyle = {
-        margin: 0,
-        height: '160px',
-        color: '#fff',
-        width:'auto',
-        lineHeight: '160px',
-        textAlign: 'center',
-        background: '#82b5e8',
-    };
-
+    const imagePaths = [
+        '/src/assets/china_1.png',
+        '/src/assets/china_2.png',
+        '/src/assets/china_3.png',
+        '/src/assets/china_4.png',
+        '/src/assets/china_5.png',
+        '/src/assets/china_6.png',
+        '/src/assets/china_7.png',
+        '/src/assets/china_8.png',
+        '/src/assets/china_9.png',
+        '/src/assets/china_10.png',
+        '/src/assets/china_11.png',
+        '/src/assets/china_12.png',
+        '/src/assets/china_13.png',
+        '/src/assets/china_14.png',
+    ]
     return (
         <>
-            <div className={styles.wrapper}>
+        <div className={styles.wrapper}>
 
-                <div className={styles.container}>
-                    <h1>{currExc.name}</h1>
-                </div>
-                <Carousel style={{ marginBottom: 100 }} arrows dotPosition="left" infinite={false}>
-                    <div>
-                        <h3 style={contentStyle}>
-                        Пример текста
-                        </h3>
-                    </div>
-                    <div>
-                        <h3 style={contentStyle}>Пример текста 2 </h3>
-                    </div>
-                    <div>
-                        <h3 style={contentStyle}>Пример текста 3 </h3>
-                    </div>
-                    <div>
-                        <h3 style={contentStyle}>Пример текста 4</h3>
-                    </div>
-                </Carousel>
-                <div className={styles.container}>
-                    <img src='/src/assets/player.svg'/>
-                </div>
-                <img />
-                <NavButton />
+            <div className={styles.container}>
+                <h1>{currExc.name}</h1>
             </div>
-        </>
+            <div className={styles.carousel}>
+                {currExc.text.map((text, index) => (
+                    <div key={index} className={styles.carouselItem}>
+                        <p className={styles.carouselText}>{text}</p>
+                        <img src={imagePaths[index]} alt={`Carousel Image ${index + 1}`} className={styles.carouselImage}/>
+                    </div>
+                ))}
+            </div>
+        </div>
+            <div className={styles.container}>
+            <img src='/src/assets/player.svg'/>
+        </div>
+        <NavButton/>
+</>
 
-    )
+)
 }
 
 export default Excurtion
