148db23d8dd2bbb6e62401973ad13f7b1100d0c9
encayni <oganisyanararat1@gmail.com>
2024-06-04 21:40:12 +0400

    fds

--- src/components/NavButton/NavButton.jsx
+++ src/components/NavButton/NavButton.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import styles from "components/NavButton/navButton.module.css"
 import { Link } from 'react-router-dom'
 
@@ -7,11 +6,11 @@ function NavButton() {
     <div className={styles.container}>
         <div className={styles.content}>
           <Link to="/">
-              <img src='/src/assets/location.png'/>
+            <img src='../../assets/location.png'/>
           </Link>
-          <img src='/src/assets/line.png'/>
+          <img src='../../assets/line.png'/>
           <Link to="/profile">
-            <img src='/src/assets/user.png'/>
+            <img src='../../assets/user.png'/>
           </Link>
         </div>
     </div>
